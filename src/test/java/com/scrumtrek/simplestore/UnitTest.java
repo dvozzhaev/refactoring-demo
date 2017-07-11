f9f40e1a0511ef62d1da12d39134ceeccd44bfe6
Dmitry Vozzhaev <dmitry.vozzhaev@gmail.com>
2017-07-11 15:08:28 +0300

    system tests -> unit tests 2

--- src/test/java/com/scrumtrek/simplestore/UnitTest.java
+++ src/test/java/com/scrumtrek/simplestore/UnitTest.java
@@ -7,22 +7,28 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UnitTest {
-    @Test
-    public void shouldAddFrequentPointsWhenNewMovieLongRented()
-    {
-        String customerName = "Customer1";
 
+    private final String customerName = "Customer1";
+
+    private Rental createRental(String movieName, PriceCodes priceCode, int daysRented) {
         Movie movieMock = mock(Movie.class);
         when(movieMock.getTitle())
-                .thenReturn("New Release Movie 1");
+                .thenReturn(movieName);
         when(movieMock.getPriceCode())
-                .thenReturn(PriceCodes.NewRelease);
+                .thenReturn(priceCode);
 
         Rental rentalMock = mock(Rental.class);
         when(rentalMock.getDaysRented())
-                .thenReturn(3);
+                .thenReturn(daysRented);
         when(rentalMock.getMovie())
                 .thenReturn(movieMock);
+        return rentalMock;
+    }
+
+    @Test
+    public void shouldAddFrequentPointsWhenNewMovieLongRented()
+    {
+        Rental rentalMock = createRental("New Release Movie 1", PriceCodes.NewRelease, 3);
 
         //Arrange
         Customer sut = new Customer(customerName);
@@ -35,5 +41,78 @@ public class UnitTest {
                 "\tNew Release Movie 1\t9.0\n" +
                 "Amount owed is 9.0\n" +
                 "You earned 2 frequent renter points.", sut.Statement());
+    }
+
+
+    @Test
+    public void shouldChargeExtraWhenRegularMovieLongRented()
+    {
+        Rental rentalMock = createRental("Regular Movie 1", PriceCodes.Regular, 5);
+
+        //Arrange
+        Customer sut = new Customer(customerName);
+
+        //Act
+        sut.addRental(rentalMock);
+
+        //Assert
+        assertEquals("Rental record for Customer1\n" +
+                "\tRegular Movie 1\t6.5\n" +
+                "Amount owed is 6.5\n" +
+                "You earned 1 frequent renter points.", sut.Statement());
+    }
+
+    @Test
+    public void shouldChargeExtraWhenChildrenMovieLongRented()
+    {
+        Rental rentalMock = createRental("Children Movie 1", PriceCodes.Childrens, 5);
+
+        //Arrange
+        Customer sut = new Customer(customerName);
+
+        //Act
+        sut.addRental(rentalMock);
+
+        //Assert
+        assertEquals("Rental record for Customer1\n" +
+                "\tChildren Movie 1\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points.", sut.Statement());
+    }
+
+    @Test
+    public void shouldNotChargeExtraWhenRegularMovieShortRented()
+    {
+        Rental rentalMock = createRental("Regular Movie 1", PriceCodes.Regular, 1);
+
+        //Arrange
+        Customer sut = new Customer(customerName);
+
+        //Act
+        sut.addRental(rentalMock);
+
+        //Assert
+        assertEquals("Rental record for Customer1\n" +
+                "\tRegular Movie 1\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points.", sut.Statement());
+    }
+
+    @Test
+    public void shouldNotChargeExtraWhenChildrenMovieShortRented()
+    {
+        Rental rentalMock = createRental("Children Movie 1", PriceCodes.Childrens, 1);
+
+        //Arrange
+        Customer sut = new Customer(customerName);
+
+        //Act
+        sut.addRental(rentalMock);
+
+        //Assert
+        assertEquals("Rental record for Customer1\n" +
+                "\tChildren Movie 1\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points.", sut.Statement());
     }
 }
